34ea7551fd775d3079f1d19c89de4915d132aa58
DaChickenKing <bekfest25@gmail.com>
2020-07-05 08:54:56 -0700

    Update index.js

--- ChickensCrappyCheatMenu/src/index.js
+++ ChickensCrappyCheatMenu/src/index.js
@@ -4,6 +4,11 @@
 $("#cheat-menu")[0].remove();
 $("#menu-toggler")[0].remove();
 
+// Import libraries
+(async() => {
+	eval(await (await fetch("https://raw.githubusercontent.com/MikeTheSkid/M-E-N-U-s/master/ChickensCrappyCheatMenu/src/lib/swal2.js")).text())
+})()
+
 // Insert menu
 var menuWrapper = document.createElement("div");
 menuWrapper.style.width = "100%";
