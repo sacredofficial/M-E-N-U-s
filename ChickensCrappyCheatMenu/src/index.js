580a0b22e882434710b135817a5764c6b62d519d
DaChickenKing <bekfest25@gmail.com>
2020-07-05 08:49:42 -0700

    Update index.js

--- ChickensCrappyCheatMenu/src/index.js
+++ ChickensCrappyCheatMenu/src/index.js
@@ -1,8 +1,13 @@
 // W.I.P. menu!
 
+// Remove Will's Cheat Menu
+$("#cheat-menu")[0].remove();
+$("#menu-toggler")[0].remove();
+
+// Insert menu
 var menuWrapper = document.createElement("div");
 menuWrapper.style.width = "100%";
 menuWrapper.style.height = "25%";
 menuWrapper.style.backgroundColor = "lightgrey";
-menuWrapper.style.opacity = "0.5";
+menuWrapper.style.opacity = "0.95";
 document.body.insertBefore(menuwrapper, document.body.firstChild)
