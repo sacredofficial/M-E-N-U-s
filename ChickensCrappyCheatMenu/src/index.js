63dac4b20062f318a90f0ec3a978548e8ced4124
DaChickenKing <bekfest25@gmail.com>
2020-07-05 08:39:20 -0700

    Update and rename ChickensCrappyCheatMenu/menu.js to ChickensCrappyCheatMenu/src/index.js

---
+++ ChickensCrappyCheatMenu/src/index.js
@@ -0,0 +1,6 @@
+// W.I.P. menu!
+
+var menuWrapper = document.createElement("div");
+menuWrapper.style.width = "100%";
+menuWrapper.styole.height = "25%";
+document.body.insertBefore(menuwrapper, document.body.firstChild)
