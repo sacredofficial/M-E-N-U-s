3d7a702e20af91d61ce4083c9f740d333c109d28
DaChickenKing <bekfest25@gmail.com>
2020-07-05 08:41:51 -0700

    Update index.js

--- ChickensCrappyCheatMenu/src/index.js
+++ ChickensCrappyCheatMenu/src/index.js
@@ -2,5 +2,7 @@
 
 var menuWrapper = document.createElement("div");
 menuWrapper.style.width = "100%";
-menuWrapper.styole.height = "25%";
+menuWrapper.style.height = "25%";
+menuWrapper.style.backgroundColor = "lightgrey";
+menuWrapper.style.opacity = "0.5";
 document.body.insertBefore(menuwrapper, document.body.firstChild)
